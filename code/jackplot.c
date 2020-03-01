149a4a9a89cd83776852cacd050b805b4b7f4952
Andre Osku Schmidt <andre@osku.de>
2020-03-01 18:57:01 +0100

    scnr

--- code/jackplot.c
+++ code/jackplot.c
@@ -1,102 +1,113 @@
-#include <stdio.h>
-#include <jack/jack.h>
 #include <GL/glu.h>
 #include <GLFW/glfw3.h>
+#include <jack/jack.h>
 
-// TODO: how bad is this code?
-// TODO: what is completely wrong?
-// TODO: whats a better solution?
-// TODO: render() is sometimes called more than once before next process() is called
-// TODO: can we feed the whole data to opengl? (without for loop)
-
-int x;
-int num_frames;
-jack_client_t *client;
-jack_port_t *input_port;
-jack_default_audio_sample_t *in;
-jack_default_audio_sample_t *last;
-jack_default_audio_sample_t *ptr;
-
-void
-render ()
-{
-	last = in + num_frames;
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POINTS);
-	x = 0;
-	for (ptr = in; ptr < last; ptr++) {
-		glVertex2f(x, *ptr);
-		x++;
-	}
-	glEnd();
-	glFlush();
-}
+int                          buffer_size;
+jack_port_t*                 jack_port;
+jack_default_audio_sample_t* jack_buffer;
 
 int
-process (jack_nframes_t nframes, void *arg)
-{
-	in = jack_port_get_buffer(input_port, nframes);
-	num_frames = nframes;
+on_jack_process (
+	jack_nframes_t nframes,
+	         void* arg
+){ //--------------------------------------------------------------------------
+	jack_buffer = jack_port_get_buffer (jack_port, nframes);
+	buffer_size = nframes;
+
 	return 0;
 }
 
-// https://github.com/iiiypuk/glfw-examples/blob/master/fps_counter.c
-int frames = 0;
-double t, t0, fps;
-char title_string[10];
-int show_fps (GLFWwindow* window)
-{
-	t = glfwGetTime();
-	if((t - t0) > 1.0 || frames == 0)
-	{
-		fps = (double)frames / (t - t0);
-		sprintf(title_string, "FPS: %.1f", fps);
-		glfwSetWindowTitle(window, title_string);
-		t0 = t;
-		frames = 0;
-	}
-	frames++;
+void
+on_opengl_resize (
+	GLFWwindow* window,
+	        int width,
+	        int height
+){ //--------------------------------------------------------------------------
+	glViewport (0, 0, width, height);
 }
 
-int
-main (void)
-{
-	GLFWwindow* window;
+void
+setup_jack (
+	jack_client_t** client
+){ //--------------------------------------------------------------------------
 	jack_status_t  status;
 	jack_options_t options = JackNullOption;
 
-	// Setup JACK
-	client = jack_client_open("jackplot", options, &status, NULL);
-	jack_set_process_callback(client, process, 0);
-	input_port = jack_port_register(client, "audio_1", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
-	jack_activate(client);
+	*client
+		= jack_client_open      ("jackplot", options, &status, NULL);
+	jack_set_process_callback   (*client, on_jack_process, 0);
+	jack_port
+		= jack_port_register    (*client, "audio_1", JACK_DEFAULT_AUDIO_TYPE,
+		                         JackPortIsInput, 0);
+	jack_activate               (*client);
+}
 
-	// Setup GLFW
-	glfwInit();
-	window = glfwCreateWindow(1024, 512, "jackplot", NULL, NULL);
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // TODO: what happens if we remove this?
+void
+setup_window (
+	GLFWwindow** window,
+	         int width
+){ //--------------------------------------------------------------------------
+	glfwInit                       ();
+	*window = glfwCreateWindow     (width, width/2, "jackplot", NULL, NULL);
+	glfwSetFramebufferSizeCallback (*window, on_opengl_resize);
+	glfwMakeContextCurrent         (*window);
+	glfwSwapInterval               (1);
+}
 
-	// Setup OpenGL
-	glColor3f(1.0, 1.0, 1.0);
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	gluOrtho2D(0, 1024, -1.0, 1.0);
-	glFlush();
-	glfwSwapBuffers(window);
+void
+setup_opengl (
+	GLFWwindow* window,
+	        int width
+){ //--------------------------------------------------------------------------
+	glColor3f       (1.0, 1.0, 1.0);
+	glClearColor    (0.0, 0.0, 0.0, 1.0);
+	glClear         (GL_COLOR_BUFFER_BIT);
+	gluOrtho2D      (0, width, -1.0, 1.0);
+	glFlush         ();
+	glfwSwapBuffers (window);
+}
 
-	// Press play on tape
-	while (!glfwWindowShouldClose(window))
-	{
-		render();
-		glfwSwapBuffers(window);
-		show_fps(window);
-		glfwPollEvents();
+void
+plot_points (
+){ //--------------------------------------------------------------------------
+	static int                          x;
+	static jack_default_audio_sample_t* y;
+	static jack_default_audio_sample_t* last;
+
+	x       = 0;
+	last    = jack_buffer + buffer_size;
+	glClear (GL_COLOR_BUFFER_BIT);
+	glBegin (GL_POINTS);
+
+	for (y = jack_buffer; y < last; y++) {
+		glVertex2f (x, *y);
+		x++;
 	}
 
-	// Done
-	jack_client_close(client);
-	glfwDestroyWindow(window);
-	glfwTerminate();
+	glEnd   ();
+	glFlush ();
+}
+
+int
+main (
+	void
+){ //--------------------------------------------------------------------------
+	GLFWwindow*    window;
+	jack_client_t* client;
+
+	setup_jack   (&client);
+	setup_window (&window, jack_get_buffer_size (client));
+	setup_opengl (window,  jack_get_buffer_size (client));
+
+	while (!glfwWindowShouldClose (window))
+	{
+		plot_points     ();
+		glfwSwapBuffers (window);
+		glfwPollEvents  ();
+	}
+
+	glfwDestroyWindow (window);
+	glfwTerminate     ();
+
 	return 0;
 }
--- readme.md
+++ readme.md
@@ -1,27 +1,23 @@
 # jackplot
 
-plot jack audio data in real-time. for fun and _"science"_...
+plot [jack](https://jackaudio.org/) audio data in real-time. for fun and _"science"_...
 
 **WARNING: as this is c - _and i dont really know what im doing_ - use at your own risk!**
 
-<img src="docu/jackplot.apng" style="background:black" title="you gotta see it live! snappy, crisp and minimal cpu usage!" />
+<img src="docu/jackplot.apng" style="background:black" title="you gotta see it live! snappy, crisp and minimal cpu usage!"/>
 
 > **SORRY** about the cpu usage of above apng, should play only once!
 
-## help
-
-- if you're a noob (like me) don't hesitate to ask "stupid" questions.
-- if you're a pro don't hesitate to (rage AND) lend a hand.
-
 ## todo
 
-0. learn c, jack, glfw and opengl :dizzy:
-1. document development
-1. window resize
-1. point/"pixel" resize? (relative to window size?)
-1. fullscreen (optional video mode?)
-1. more input channels
-1. channel plot color
+- [ ] learn c, jack, glfw and opengl :dizzy:
+- [ ] document development
+- [x] window resize
+- [ ] data resize?
+- [ ] point/"pixel" resize? (relative to window size?)
+- [x] ~~fullscreen~~ : use your window manager
+- [ ] more input channels
+- [ ] channel plot color
 
 ## ideas
 
